a5fbe6bf4bd36179cb575e8d09cd062b085f392e
Atil Samancioglu <samancioglu.atil@gmail.com>
2023-11-21 21:40:25 +0300

    do one thing principle added

---
+++ app/src/main/java/com/atilsamancioglu/annotationmastery/cleancodeexamples/DoOneThingExample.kt
@@ -0,0 +1,37 @@
+package com.atilsamancioglu.annotationmastery.cleancodeexamples
+
+//wrong way
+fun processNumbers(numbers: List<Int>) {
+    val sum = numbers.sum()
+    val max = numbers.maxOrNull() ?: 0
+
+    println("Sum: $sum")
+    println("Max: $max")
+}
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5)
+    processNumbers(numbers)
+}
+
+//right way
+
+fun sumNumbers(numbers: List<Int>): Int {
+    return numbers.sum()
+}
+
+fun findMax(numbers: List<Int>): Int {
+    return numbers.maxOrNull() ?: 0
+}
+
+fun displayResults(sum: Int, max: Int) {
+    println("Sum: $sum")
+    println("Max: $max")
+}
+
+fun main() {
+    val numbers = listOf(1, 2, 3, 4, 5)
+    val sum = sumNumbers(numbers)
+    val max = findMax(numbers)
+    displayResults(sum, max)
+}
